1ecbdfcbfabc42294fc70ae3531f13ea77796dc6
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2024-01-30 07:12:00 +0000

    type array

---
+++ src/array.ts
@@ -0,0 +1,12 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let string: string[] = ['TypeScript', 'JavaScript', 'CoffeeScript'];
+
+let nijigenHairetsu: number[][] = [
+  [10, 20],
+  [30, 40],
+];
+
+let hairetsu: (string | number | boolean)[] = [1, false, 'Japan'];
