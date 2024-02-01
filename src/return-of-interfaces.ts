564fe0fa35b375a1a9de129b89b7b7b8db7708b0
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2024-02-01 08:43:33 +0000

    interfaces

---
+++ src/return-of-interfaces.ts
@@ -0,0 +1,27 @@
+export {};
+
+class Mahotsukai {}
+
+class Souryo {}
+
+class Taro extends Mahotsukai {}
+
+interface Kenja {
+  ionazun(): void;
+}
+
+interface Senshi {
+  kogeki(): void;
+}
+
+class Jiro implements Kenja, Senshi {
+  ionazun(): void {
+    console.log('ionazun');
+  }
+  kogeki(): void {
+    console.log('kogeki');
+  }
+}
+
+const jiro = new Jiro();
+jiro.ionazun();
