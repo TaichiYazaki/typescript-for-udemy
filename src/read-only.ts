77eb2579fbc10110f3c83b934b312f702e0a1b2d
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2024-02-01 07:36:42 +0000

    read-only

---
+++ src/read-only.ts
@@ -0,0 +1,10 @@
+export {};
+
+class VisaCard {
+  constructor(readonly owner: string) {}
+}
+
+let myVisaCard = new VisaCard('Taro');
+console.log(myVisaCard.owner);
+
+// myVisaCard.owner = 'Tim';
